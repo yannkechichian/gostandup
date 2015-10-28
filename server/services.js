07ec80c272d0c08bb7f114abe0260bffa523b169
Yann Kechichian <yann.kechichian@gmail.com>
2015-10-28 12:32:51 +1100

    Remove credentials

--- server/services.js
+++ server/services.js
@@ -1,17 +1,10 @@
 ServiceConfiguration.configurations.upsert(
   { service: "github" },
   {
-  	// production
     $set: {
-      clientId: "cf20ae79f2c2d1452b97",
+      clientId: "EDITME",
       loginStyle: "redirect",
-      secret: "f5ea5c3180905baf295f346881b1646e2e29878c"
+      secret: "EDITME"
     }
-    // development
-    // $set: {
-    //   clientId: "ae439f41fbaf90a183b2",
-    //   loginStyle: "redirect",
-    //   secret: "52f2109509c3e92cf0f4aa273bf6e67c3484741e"
-    // }
   }
 );
